bf0d3236a370ed70ee552b2b5fd2864569ce2b86
you <kutcnikita9@gmail.ru>
2023-03-29 18:24:53 +0300

    2

--- src/main/java/base/Register.java
+++ src/main/java/base/Register.java
@@ -1,13 +1,18 @@
 package base;
 
 import io.qameta.allure.Step;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
 import pojo.User;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
 
 public class Register extends RestClient {
     private static final String Register_URI = RestClient.BASE_URI + "auth/register";
+    private static final String Auth_URI = RestClient.BASE_URI + "auth/login";
+    private static final String Delite_URI = RestClient.BASE_URI + "auth/user";
 
     @Step("Создание пользователя")
     public ValidatableResponse createUser(User user){
@@ -19,10 +24,30 @@ public class Register extends RestClient {
     }
 
     @Step("Авторизация")
-    public ValidatableResponse userAuth(User name){
+    public ValidatableResponse userAuth(User user){
         return given()
-                .spec()
-                .body()
+                .spec(getReqSpec())
+                .body(user)
+                .when()
+                .post(Auth_URI)
+                .then();
     }
-
+    @Step("Токен")
+    public ValidatableResponse token(User user){
+        return  userAuth(user)
+                .statusCode(SC_OK)
+                .extract()
+                .path("accessToken");
+    }
+    @Step("Ужаление")
+    public ValidatableResponse delete(User user){
+        return  given()
+                .auth()
+                .oauth2(token(user))
+                .when()
+                .delete(Delite_URI)
+                .then()
+                .assertThat()
+                .statusCode(SC_OK);
+    }
 }
--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -1,4 +1,64 @@
+import base.Register;
+import io.qameta.allure.junit4.DisplayName;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import pojo.User;
+import static org.apache.http.HttpStatus.SC_FORBIDDEN;
+import static org.hamcrest.Matchers.equalTo;
+import static org.apache.http.HttpStatus.SC_OK;
+
 public class UserTests {
+    private User user;
+    private Register register;
 
+    @Before
+    public void setUp(){
+        register = new Register();
+        user = new User("hf@mail.ru","123456","Nikita");
+    }
 
+    @Test
+    @DisplayName("Создание уникального пользователя")
+    public void checkNewUserregister(){
+        register.createUser(user)
+                .assertThat()
+                .statusCode(SC_OK)
+                .and()
+                .body("success",equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя, который уже зарегистрирован")
+    public void checkCreateRepeatedUser(){
+        register.createUser(user)
+                .assertThat()
+                .statusCode(SC_OK);
+        User secondUser = new User(user.getEmail(), "123456","Повтор");
+        register.createUser(secondUser)
+                .assertThat()
+                .statusCode(SC_FORBIDDEN)
+                .and()
+                .body("message",equalTo("User already exists"));
+    }
+    @Test
+    @DisplayName("Логин под существующим пользователем")
+    public void checkUserAuth(){
+        register.createUser(user)
+                .assertThat()
+                .statusCode(SC_OK);
+        register.userAuth(new User(user.getEmail(), user.getPassword()))
+                .assertThat()
+                .statusCode(SC_OK)
+                .and()
+                .body("success",equalTo(true));
+    }
+    @Test
+    public void deleteUser(){
+        register.userAuth(user)
+                        .extract()
+                        .path("accessToken");
+        register.delete(user);
+    }
 }
